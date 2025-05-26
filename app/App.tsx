578bb3b70d02e9069b0a65dd8e2d0f67f740afdd
Kevin Barasa <kevin.barasa@gmgcolor.com>
2025-05-26 22:22:42 +0200

    Refactor App.tsx for improved readability and formatting
    Reorganized code structure, improved formatting, and applied consistent spacing for better readability. Removed commented-out sections and unnecessary code noise to enhance maintainability while preserving functionality.

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,3 @@
-/* eslint-disable prettier/prettier,@typescript-eslint/no-unused-vars */
-/**
- * Initially generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-//key
-//sd - self described
 /**
  * @authored by Kaybarax
  * Twitter @_ https://twitter.com/Kaybarax
@@ -17,152 +8,135 @@
 import React from 'react';
 import RN from 'react-native';
 import 'mobx-react-lite/batchingForReactNative';
-import {Provider} from 'mobx-react';
+import { Provider } from 'mobx-react';
 import AppEntry from './app-entry';
 import appStores from './stores/index';
 import appNavigation from './routing-and-navigation/app-navigation';
-import {appSQLiteDb} from './app-management/data-manager/embeddedDb-manager';
+import { appSQLiteDb } from './app-management/data-manager/embeddedDb-manager';
 import Loader from './shared-components-and-modules/loaders';
 import NotFound from './views/not-found';
 import className from './util/react-native-based-utils';
-import {FlexColumnContainerCN} from './theme/app-layout-styles-classnames';
-import {serviceWorkerThread} from './controllers/app-controller';
-import {TIME_OUT} from './app-config';
-import {isEmptyObject, isEmptyString} from './util/util';
+import { FlexColumnContainerCN } from './theme/app-layout-styles-classnames';
+import { serviceWorkerThread } from './controllers/app-controller';
+import { TIME_OUT } from './app-config';
+import { isEmptyObject, isEmptyString } from './util/util';
 import SafeComponentWrapper from './safe-component-wrapper';
 
 export const SCREEN_HEIGHT = RN.Dimensions.get('window').height;
 export const SCREEN_WIDTH = RN.Dimensions.get('window').width;
 
 const App = () => {
-
-    //manage runtime errors, warnings and logs feedback
-    if (__DEV__) {
-
-        // @ts-ignore
-        if (global.location && global.location.pathname.includes('/debugger-ui')) {
-
-            // RN.Alert.alert('Remote debug is ON');
-            //hide in-development unnecessary console warnings. Comment to display
-            // console.warn = console.error = function (message) {
-            //     // do nothing
-            // };
-
-            //disable all warnings, errors, and logs when
-            //remote debugging during development. Comment to enable
-            // console.warn = console.error = console.log = function (message) {
-            //     //do nothing
-            // };
-
-        } else {
-            // RN.Alert.alert('Remote debug is OFF');
-            //disable all warnings, errors, and logs when not
-            //remote debugging during development
-            console.warn = console.error = console.log = function (message) {
-                //do nothing
-            };
-        }
-
+  //manage runtime errors, warnings and logs feedback
+  if (__DEV__) {
+    // @ts-ignore
+    if (global.location && global.location.pathname.includes('/debugger-ui')) {
+      // RN.Alert.alert('Remote debug is ON');
+      //hide in-development unnecessary console warnings. Comment to display
+      // console.warn = console.error = function (message) {
+      //     // do nothing
+      // };
+      //Disable all warnings, errors, and logs when
+      // remotely debugging during development. Comment to enable
+      // console.warn = console.error = console.log = function (message) {
+      //     //do nothing
+      // };
     } else {
-        //disable all warnings, errors, and logs in release
-        console.warn = console.error = console.log = function (message) {
+      // RN.Alert.alert('Remote debug is OFF');
+      //disable all warnings, errors, and logs when not
+      //remote debugging during development
+      console.warn =
+        console.error =
+        console.log =
+          function (message) {
             //do nothing
+          };
+    }
+  } else {
+    //disable all warnings, errors, and logs in release
+    console.warn =
+      console.error =
+      console.log =
+        function (message) {
+          //do nothing
         };
-    }
+  }
 
-    let [dbLoaded, loadDb] = React.useState(false);
-    let [dbLoadFeedback, setDbLoadFeedback] = React.useState('');
-    let [loadAppStores, setAppStoresLoaded] = React.useState(false);
-    let [loadAppStoresFeedback, setAppStoresLoadedFeedback] = React.useState('Initializing app state...');
+  let [dbLoaded, loadDb] = React.useState(false);
+  let [dbLoadFeedback, setDbLoadFeedback] = React.useState('');
+  let [loadAppStores, setAppStoresLoaded] = React.useState(false);
+  let [loadAppStoresFeedback, setAppStoresLoadedFeedback] = React.useState('Initializing app state...');
 
-    React.useEffect(() => {
+  React.useEffect(() => {
+    console.log('appSQLiteDb.dbLoadedAndInitialized :::: ', appSQLiteDb.dbLoadedAndInitialized);
 
-        console.log('appSQLiteDb.dbLoadedAndInitialized :::: ', appSQLiteDb.dbLoadedAndInitialized);
-
-        //init embedded app db
-        serviceWorkerThread(
-            _ => {
-                appSQLiteDb.loadAndInitDB();
-            },
-            _ => {
-                return appSQLiteDb.dbLoadedAndInitialized;
-            },
-            _ => {
-                loadDb(true);
-            },
-            _ => {
-                setDbLoadFeedback('Failed to load app sqlite-db. Restart app to try again.')
-            }, TIME_OUT, 1000
-        );
-
-        //init app stores
-        serviceWorkerThread(
-            _ => {
-                appStores.loadAppStores().then(null);
-            },
-            _ => {
-                return appStores.appStoresLoaded;
-            },
-            _ => {
-                setAppStoresLoaded(true);
-                // @ts-ignore
-                // pass navStore reference to appNavigation
-                appNavigation.navStore = appStores.stores.appStore.navStore;
-            },
-            _ => {
-                setAppStoresLoaded(false);
-                setAppStoresLoadedFeedback('Failed to load app state. Restart to try again');
-            }, TIME_OUT, 1000
-        );
-
-    });
-
-    if (!dbLoaded) {
-        return (
-            <RN.ScrollView
-                style={[
-                    className(
-                        FlexColumnContainerCN
-                    )
-                ]}
-            >
-                <NotFound/>
-                <Loader message={
-                    isEmptyString(dbLoadFeedback) ?
-                        appSQLiteDb.latestProgressUpdate :
-                        dbLoadFeedback
-                }/>
-            </RN.ScrollView>
-        )
-    }
-
-    if (!loadAppStores || isEmptyObject(appStores.stores)) {
-        return (
-            <RN.ScrollView
-                style={[
-                    className(
-                        FlexColumnContainerCN
-                    )
-                ]}
-            >
-                <NotFound/>
-                <Loader message={loadAppStoresFeedback}/>
-            </RN.ScrollView>
-        )
-    }
-
-    const stores = appStores.stores;
-
-    return (
-        <Provider
-            {...stores}
-        >
-            <SafeComponentWrapper>
-                <AppEntry/>
-            </SafeComponentWrapper>
-        </Provider>
+    //init embedded app db
+    serviceWorkerThread(
+      _ => {
+        appSQLiteDb.loadAndInitDB();
+      },
+      _ => {
+        return appSQLiteDb.dbLoadedAndInitialized;
+      },
+      _ => {
+        loadDb(true);
+      },
+      _ => {
+        setDbLoadFeedback('Failed to load app sqlite-db. Restart app to try again.');
+      },
+      TIME_OUT,
+      1000,
     );
 
+    //init app stores
+    serviceWorkerThread(
+      _ => {
+        appStores.loadAppStores().then(null);
+      },
+      _ => {
+        return appStores.appStoresLoaded;
+      },
+      _ => {
+        setAppStoresLoaded(true);
+        // @ts-ignore
+        // pass navStore reference to appNavigation
+        appNavigation.navStore = appStores.stores.appStore.navStore;
+      },
+      _ => {
+        setAppStoresLoaded(false);
+        setAppStoresLoadedFeedback('Failed to load app state. Restart to try again');
+      },
+      TIME_OUT,
+      1000,
+    );
+  });
+
+  if (!dbLoaded) {
+    return (
+      <RN.ScrollView style={[className(FlexColumnContainerCN)]}>
+        <NotFound />
+        <Loader message={isEmptyString(dbLoadFeedback) ? appSQLiteDb.latestProgressUpdate : dbLoadFeedback} />
+      </RN.ScrollView>
+    );
+  }
+
+  if (!loadAppStores || isEmptyObject(appStores.stores)) {
+    return (
+      <RN.ScrollView style={[className(FlexColumnContainerCN)]}>
+        <NotFound />
+        <Loader message={loadAppStoresFeedback} />
+      </RN.ScrollView>
+    );
+  }
+
+  const stores = appStores.stores;
+
+  return (
+    <Provider {...stores}>
+      <SafeComponentWrapper>
+        <AppEntry />
+      </SafeComponentWrapper>
+    </Provider>
+  );
 };
 
 export default App;
