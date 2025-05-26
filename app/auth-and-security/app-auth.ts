172b942323606a485562010b1addbbf73c00332a
Kevin Barasa <kevin.barasa@gmgcolor.com>
2025-05-26 16:00:33 +0200

    Convert `app-auth.js` to TypeScript as `app-auth.ts`
    The file `app-auth.js` was migrated to TypeScript with the new name `app-auth.ts`. This enhances type safety and improves code maintainability by introducing explicit types such as `Promise<boolean>`. The functionality and structure of the authentication logic remain unchanged.

--- app/auth-and-security/app-auth.ts
+++ app/auth-and-security/app-auth.ts
@@ -18,9 +18,9 @@ export default class AppAuth {
   /**
    * Your frontend app, authentication logic for access: JWT, AWS Cognito, Google sign in,
    * facebook-sign-in, Twitter sign in ...  your fancy
-   * @returns {Promise<*>}
+   * @returns {Promise<boolean>}
    */
-  isAuthenticated = async () => {
+  isAuthenticated = async (): Promise<boolean> => {
     // TODO: write you authentication logic here that will need to pass for a user to
     //  access a secured page. For example if you were using aws, and a user should only
     //  access a page if still logged in, you would do something like the below commented out code:
